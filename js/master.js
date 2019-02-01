5fc3a7f9c33c5ea1f216ce7b09d38e9941b3b4ab
Darrion Ramdin <darrionr007@gmail.com>
2019-02-01 13:07:28 -0800

    Added Documentation

--- js/master.js
+++ js/master.js
@@ -1,10 +1,13 @@
 $(function() {
 	"use strict";
+
+	// data-image functionality
 	const imageElements = $('[data-image]');
     for(let element of imageElements) {
         $(element).css("background-image",`url(${$(element).data('image')})`);
     }
 
+    // Smooth Scrolling
     $('.navbar .nav-link, .scroll-link').on("click", function (e) {
         var anchor = $(this);
         $('html, body').stop().animate({
@@ -13,5 +16,16 @@ $(function() {
         e.preventDefault();
         
     });
+
+	// Animate on scroll
+    AOS.init({
+    	delay: 300,
+    	once: true
+    });
+
+    // Portfolio lightbox - remove to return to default linking (no lightbox)
+    $('.portfolio-grid a, .portfolio-grid-normal a').featherlight({
+    	targetAttr: 'data-image'
+    })
 
 })
